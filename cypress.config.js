ef489466fcb133ed17944cdd4f96686540889c2d
Marina Canuto <maricanuto01.pt@mail.com>
2026-04-20 16:57:23 -0300

    chore: atualiza configurações do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -4,9 +4,10 @@ module.exports = defineConfig({
   reporter: "mochawesome",
   reporterOptions: {
     reportDir: "cypress/reports",
-    overwrite: true,
-    html: true,
-    json: true
+    overwrite: false,
+    html: false,
+    json: true,
+    reportFilename: "[name]-[hash]"
   },
 
   e2e: {
